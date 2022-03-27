b59071697c51469e9076c22c3de3e635b4d5c027
Peter Strick <anitastriclk@gmail.com>
2022-03-27 10:02:23 +0200

    Change Version to 1.6.0.3 for AutoUpdater

--- ViVeTool-GUI.FeatureScanner/My Project/AssemblyInfo.vb
+++ ViVeTool-GUI.FeatureScanner/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.6.0.2")>
-<Assembly: AssemblyFileVersion("1.6.0.2")>
+<Assembly: AssemblyVersion("1.6.0.3")>
+<Assembly: AssemblyFileVersion("1.6.0.3")>
